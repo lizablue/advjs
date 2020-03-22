366f928d9fc1c04d4da1f9c189aa1bada52e3afe
ELIZABETH <EMCCREIGHT90@GMAIL.COM>
2020-03-22 18:23:30 -0500

    complete hoisting exercises

--- scripts.js
+++ scripts.js
@@ -7,3 +7,10 @@ function setName() {
     var name = 'liz';
     console.log(name);
 }
+console.log('average');
+let avg = findAvg(2, 2);
+console.log('average = ', avg);
+function findAvg(a, b) {
+    var answer = (a+b)/2;
+    return answer;
+}
